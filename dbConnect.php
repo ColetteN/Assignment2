4dd167f7e96418aaaeab1d6d65a6fb15612670ea
ColetteN <k00212038@student.lit.ie>
2018-11-18 15:12:36 +0000

    Update

--- dbConnect.php
+++ dbConnect.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ColetteNash
- * Date: 18/11/2018
- * Time: 12:45
- */
+//connect here to the database
+try {   $conn = new PDO('mysql:host=localhost;dbname=shoppingcart', 'root');//connect here to the database
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//for errors
+}
+catch(PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
+//this will be equal to $_POST when grabbing data from a form
+$email = 'colettenash.webdeveloper@gmail.com';
+
+//prepared statement to select from database
+$stmt=$conn->prepare('SELECT * FROM userlogin WHERE email=:email');
+$stmt->execute(['email'=>$email]);
+$user=$stmt->fetch();
+
+//var_dump($user);
+
+//Get array containing all of the result rows
+$stmt=$conn->query('SELECT * FROM userlogin');
+$result = $stmt->fetchAll();
+foreach($result as $row) {
+    print_r($row);
+}
--- login.php
+++ login.php
@@ -1,1 +1,29 @@
 <?php
+include("dbConnect.php");
+include("header.php");
+session_start();
+?>
+<html>
+
+<body>
+
+<div id="main">
+    <div id="login">
+      <h1>LOGIN</h1>
+
+        <div id="inner" >
+
+            <form action = "" method = "post">
+                <label>UserName  :</label><br /><input type = "text" name = "username" /><br /><br />
+                <label>Password  :</label><br /><input type = "text" name = "password" /><br/><br />
+                <input type = "submit" value = " Submit "/><br />
+            </form>
+
+        </div>
+
+    </div>
+
+</div>
+
+</body>
+</html>
