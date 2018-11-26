d00969ac38b968e0aa517741f05dd790210a7893
ColetteN <k00212038@student.lit.ie>
2018-11-26 17:59:06 +0000

    Update

--- shopping.php
+++ shopping.php
@@ -10,28 +10,28 @@ $stmt = $conn->prepare('SELECT * FROM userlogin WHERE username=:username AND pas
 $stmt->execute(['username'=> $username, 'password'=> $password]);
 $user = $stmt->fetch();
 //var_dump($user);
-echo'
-<div class="gallery">
-    <img src="images/camera.jpg" alt="Camera" width="300" height="200">
-  </a>
-  <div class="desc">Add a description of the image here</div>
+?>
+
+<div class="product">
+<?php
+    //get all product images from the db table
+    //this is returned as an array
+    //loop through the array
+    $sql = "SELECT * FROM products";
+    $stmt = $conn->query($sql);
+
+while($row = $stmt->fetch()) {
+    echo '<img src="images/watch.jpg?id='  . $row["p_id"] .  '">' . $row["p_name"] . '"/>';
+    var_dump($row);
+}
+
+?>
+
 </div>
 
-<div class="gallery">
-    <img src="images/drive.jpg" alt="Hard Drive" width="300" height="200">
-  </a>
-  <div class="desc">Add a description of the image here</div>
-</div>
 
-<div class="gallery">
-    <img src="images/watch.jpg" alt="Watch" width="300" height="200">
-  </a>
-  <div class="desc">Add a description of the image here</div>
-</div>
-
-<div class="gallery">
-    <img src="images/laptop.jpg" alt="Mountains" width="300" height="200">
-  </a>
-  <div class="desc">Add a description of the image here</div>
-</div>';
+<!---->
+<!--<div class="product">-->
+<!--    <img src="image/camera.jpg" alt="Camera" width="300" height="200">-->
+<!--</div>-->
 
