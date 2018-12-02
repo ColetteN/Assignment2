3b3d61921d28c016f77883b2a55eb59cf471b6b8
ColetteN <k00212038@student.lit.ie>
2018-12-02 17:47:37 +0000

    Update

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+include("dbConnect.php");
+session_start();
+
+function LogIn()
+{
+    //session_start();
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if(!empty($_POST['username']))   //checking
+    {
+        //connect here to the database
+        //$conn = new PDO('mysql:host=localhost;dbname=shoppingcart', 'root', '');
+        $stmt = $conn->prepare('SELECT * FROM userlogin WHERE username=:username AND password=:password');
+        $stmt->execute(['username'=> $username, 'password'=> $password]);
+        $user = $stmt->fetch();
+
+        if(!empty($row['username']) AND !empty($row['password']))
+        {
+            $_SESSION['username'] = $row['password'];
+            echo "Welcome";
+
+        }
+        else
+        {
+            echo "Wrong username or password";
+        }
+    }
+
+}
+if(isset($_POST['submit']))
+{
+    LogIn();
+}
--- login.php
+++ login.php
@@ -1,42 +1,9 @@
 <?php
 include("dbConnect.php");
 include("header.php");
+include ("functions.php");
+
 session_start();
-?>
-
-<?php
-function LogIn()
-{
-    //session_start();
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-
-    if(!empty($_POST['username']))   //checking
-    {
-        //connect here to the database
-        //$conn = new PDO('mysql:host=localhost;dbname=shoppingcart', 'root', '');
-        $stmt = $conn->prepare('SELECT * FROM userlogin WHERE username=:username AND password=:password');
-        $stmt->execute(['username'=> $username, 'password'=> $password]);
-        $user = $stmt->fetch();
-
-        if(!empty($row['username']) AND !empty($row['password']))
-        {
-            $_SESSION['username'] = $row['password'];
-            echo "Welcome";
-
-        }
-        else
-        {
-            echo "Wrong username or password";
-        }
-    }
-}
-
-if(isset($_POST['submit']))
-{
-    LogIn();
-}
 ?>
 
 <body>
